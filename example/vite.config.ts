910f781bb0d513ebad7d0695ad9683f9157de19f
MacroxW <wendymarcos2@gmail.com>
2026-05-26 15:44:50 -0300

    resolve library

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/react-insta-collage/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      react: fileURLToPath(new URL('./node_modules/react', import.meta.url)),
+      'react-dom': fileURLToPath(new URL('./node_modules/react-dom', import.meta.url)),
+    },
+    dedupe: ['react', 'react-dom'],
+  },
 })
